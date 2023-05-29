ea4cb0be3d8daaeb40b8f5b25b7f76daca041dc9
nameismani <120552594+nameismani@users.noreply.github.com>
2023-05-29 17:17:14 +0530

    fixed the bug

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-let signIn = document.getElementById("signIn");
-let signUp = document.getElementById('signUp');
+// let signIn = document.getElementById("signIn");
+// let signUp = document.getElementById('signUp');
 let container = document.getElementById("container");
 
-signUp.addEventListener('click',()=>{
-    container.classList.add("right-panel-active");
-})
+// signUp.addEventListener('click',()=>{
+//     container.classList.add("right-panel-active");
+// })
 
-signIn.addEventListener('click',()=>{
-    container.classList.remove("right-panel-active");
-})
+// signIn.addEventListener('click',()=>{
+//     container.classList.remove("right-panel-active");
+// })
 
-console.log(container)
+// console.log(container)
+function signIn() {
+    container.classList.remove("rigth-panel-active");
+}
+
+function signUp(){
+    container.classList.add("rigth-panel-active");
+}
